09f204bcbe825f4c799d0165ecca42bcdab9e990
amako@centroida.ai <amako@centroida.ai>
2018-11-26 00:20:00 +0200

    no modules pls and no dist

--- flightsService/dist/flight.service.ts/startFlightService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const flight_service_1 = require("./flight.service");
-const startFlightService = () => {
-    const flightService = new flight_service_1.default();
-};
-startFlightService();
-//# sourceMappingURL=startFlightService.js.map
--- reservation-service/dist/reservation.service.ts/startReservationService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const reservation_service_1 = require("./reservation.service");
-const startReservationService = () => {
-    const reservationService = new reservation_service_1.default();
-};
-startReservationService();
-//# sourceMappingURL=startReservationService.js.map
